1aef65f26cbdb760117af62b407a88992debf5b6
legendofzara <c21zc@dalton.org>
2018-01-29 11:33:43 -0500

    wrong an epic drama for the ages

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,8 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Alexa Taylor Fox");
+        System.out.println("'hey, zara, guess what?'");
+        System.out.println("'-what?'");
+        System.out.println("'nO oNe WaNtS yOuR pReP fReShMen'");
     }
 }
