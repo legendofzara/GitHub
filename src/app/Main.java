4b68899aedc7ec722388e7b1c7a31879623458f8
legendofzara <c21zc@dalton.org>
2018-01-29 11:34:04 -0500

    wrong an epic drama for the ages 6

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("'hey, zara, guess what?'");
         System.out.println("'-what?'");
-        System.out.println("'nO oNe WaNtS yOuR pReP fReShMen'");
+        System.out.println("'nO oNe WaNtS yOuR pReP fReShMen' ");
     }
 }
